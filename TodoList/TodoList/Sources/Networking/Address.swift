3b14bddd0c4cadcc7ae75d21630a8fe61e78e15d
oh_jun <vhzkclq0705@naver.com>
2022-11-20 21:50:50 +0900

    네트워킹 준비 파일 추가

---
+++ TodoList/TodoList/Sources/Networking/Address.swift
@@ -0,0 +1,24 @@
+//
+//  Address.swift
+//  TodoList
+//
+//  Created by 권오준 on 2022/11/20.
+//
+
+import Foundation
+
+enum Address {
+    // Member
+    case signUp
+    case login
+    case allMember
+    case member
+    case updateMember
+    case deleteMember
+    // Todo
+    case allTodos
+    case todo
+    case createTodo
+    case updateTodo
+    case deleteTodo
+}
---
+++ TodoList/TodoList/Sources/Networking/ServiceAPI.swift
@@ -0,0 +1,10 @@
+//
+//  ServiceAPI.swift
+//  TodoList
+//
+//  Created by 권오준 on 2022/11/20.
+//
+
+import Alamofire
+import Foundation
+
