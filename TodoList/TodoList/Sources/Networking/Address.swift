01e101dd98b549e8e6a0455232861def15be31f5
oh_jun <vhzkclq0705@naver.com>
2022-11-21 11:45:47 +0900

    Address 수정

--- TodoList/TodoList/Sources/Networking/Address.swift
+++ TodoList/TodoList/Sources/Networking/Address.swift
@@ -5,12 +5,14 @@
 //  Created by 권오준 on 2022/11/20.
 //
 
+import Alamofire
 import Foundation
 
 enum Address {
-    // Member
+    // Login
     case signUp
     case login
+    // Member
     case allMember
     case member
     case updateMember
@@ -21,4 +23,57 @@ enum Address {
     case createTodo
     case updateTodo
     case deleteTodo
+}
+
+extension Address {
+    var url: String {
+        switch self {
+            // Login
+        case .signUp: return "members/sign-up"
+        case .login: return "members/sign-in"
+            
+            // Member
+        case .allMember: return "members"
+        case .member,
+                .updateMember,
+                .deleteMember:
+            return "members/" // + memberID
+            
+            // Todo
+        case .allTodos,
+                .createTodo:
+            return "todos"
+        case .todo,
+                .updateTodo,
+                .deleteTodo:
+            return "todos/" // + TodoID
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+            // GET
+        case .allMember,
+                .member,
+                .allTodos,
+                .todo:
+            return .get
+            
+            // POST
+        case .signUp,
+                .login,
+                .createTodo:
+            return .post
+            
+            // PATCH
+        case .updateMember,
+                .updateTodo:
+            return .patch
+            
+            // DELETE
+        case .deleteMember,
+                .deleteTodo:
+            return .delete
+        }
+    }
 }
